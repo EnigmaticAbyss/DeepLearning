14eeb6dc2d1485bc4a9dba2acbdb4ff16e68b0f2
behrooz bozorg chami <behroozbc@outlook.com>
2024-05-22 19:15:12 +0200

    softmax

--- exercise1_material/src_to_implement/Layers/SoftMax.py
+++ exercise1_material/src_to_implement/Layers/SoftMax.py
@@ -1,18 +1,16 @@
 import numpy as np
 from Layers.Base import BaseLayer
 class SoftMax(BaseLayer):
+    def __init__(self):
+        super().__init__()
+    
     def forward(self,input_tensor):
         result=[]
         for i in input_tensor:
             i-=i.max()
             exponential = np.exp(i)
             result.append(exponential / np.sum(exponential))
-        return np.array(result)
+        self.ouput=np.array(result)
+        return self.ouput
     def backward(self,error_tensor):
-        rest=np.zeros_like(error_tensor)
-        for i in error_tensor:
-            i-=i.max()
-            exponential = np.exp(i)     
-            y=exponential / np.sum(exponential)
-            rest[i]=(y*(i- i*y))
-        return rest
+        return self.ouput * (error_tensor - (error_tensor * self.ouput).sum(axis = 1)[np.newaxis].T)
