41bc9427b0e5e0e09350ef5a7abfcca761564597
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-06-13 15:41:52 +0200

    test

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -54,7 +54,7 @@ class Conv:
                         # print("weigh tens")
                         # print("padded_input")
                         # print(scipy.signal.correlate(padded_input, self.weights[k, c], mode='valid').shape)
-                        # print(scipy.signal.correlate(padded_input, self.weights[k, c], mode='valid')[::self.stride_shape[0]].shape)
+
                         output_tensor[b, k] += scipy.signal.correlate(padded_input, self.weights[k, c], mode='valid')[::self.stride_shape[0]]
                     output_tensor[b, k] += self.bias[k]  # Adding the bias element-wise
 
@@ -74,7 +74,24 @@ class Conv:
                     output_tensor[b, k] += self.bias[k]  # Adding the bias element-wise
 
         return output_tensor
+    # def backward(self, error_tensor):
+    #     batch_size, channels, *spatial_dims = self.input_tensor.shape
+    #     conv_dims = self.convolution_shape[1:]  # Get spatial dimensions of the convolution filter
 
+    #     self.gradient_weights = np.zeros_like(self.weights)
+    #     self.gradient_bias = np.zeros_like(self.bias)
+    #     gradient_input = np.zeros_like(self.input_tensor)
+
+    #     if len(conv_dims) == 1:  # 1D convolution
+    #         pad = (conv_dims[0] // 2, conv_dims[0] // 2) if conv_dims[0] % 2 == 1 else (conv_dims[0] // 2, conv_dims[0] // 2 - 1)
+
+    #         for b in range(batch_size):
+    #             for k in range(self.num_kernels):
+    #                 for c in range(channels):
+    #                     padded_input = np.pad(self.input_tensor[b, c], pad, mode='constant')
+    #                     self.gradient_weights[k, c] += scipy.signal.convolve(padded_input, error_tensor[b, k], mode='valid')
+    #                     gradient_input[b, c] += scipy.signal.correlate(error_tensor[b, k], self.weights[k, c], mode='full')[pad[0]:-pad[1] or None]
+    #                 self.gradient_bias[k] += np.sum(error_tensor[b, k])
 
     def backward(self, error_tensor):
         batch_size, channels, *spatial_dims = self.input_tensor.shape
@@ -90,32 +107,36 @@ class Conv:
             for b in range(batch_size):
                 for k in range(self.num_kernels):
                     for c in range(channels):
-                        print("error tens")
-                        print(error_tensor.shape)
-                        print("grad shape")
-                        print(self.convolution_shape)
-                        print(self.gradient_weights.shape)
-                        print("input_tensor")
-                        print(self.input_tensor.shape)
-                        print("no pad")
-                        print(self.input_tensor[b, c])
+                        # print("error tens")
+                        # print(error_tensor.shape)
+                        # print("grad shape")
+                        # print(self.convolution_shape)
+                        # print(self.gradient_weights.shape)
+                        # print("input_tensor")
+                        # print(self.input_tensor.shape)
+                        # print("no pad")
+                        # print(self.input_tensor[b, c])
                         padded_input = np.pad(self.input_tensor[b, c], pad, mode='constant')
-                        error_resized = error_tensor[b, k].reshape(-1) 
-                        print("error resized")# Ensure error tensor has the correct shape
-                        print(error_resized)
-                        print("self gradient:")
-                        print(self.gradient_weights[k, c].shape)
-                        print("error")
-                        print(error_tensor[b, k].shape)
-                        print("padded_input")
+                        # error_resized = error_tensor[b, k].reshape(-1) 
+                        # print("error resized")# Ensure error tensor has the correct shape
+                        # print(error_resized)
+                        # print("self gradient:")
+                        # print(self.gradient_weights[k, c].shape)
+                        # print("error")
+                        # print(error_tensor[b, k].shape)
+                        # print("padded_input")
                         
-                        print(padded_input.shape)
-                        print("amount")
-                        print((scipy.signal.correlate(padded_input,error_tensor[b, k] , mode='valid')).shape)
-                        self.gradient_weights[k,c ] += scipy.signal.correlate(padded_input, error_tensor[b, k], mode='valid')
+                        # print(padded_input.shape)
+                        # print("amount")
+                        # print((scipy.signal.correlate(padded_input,error_tensor[b, k] , mode='valid')).shape)
+                        print(self.gradient_weights.shape)
+                        print("sss")
+                        print(scipy.signal.correlate(padded_input, error_tensor[b, k], mode='valid').shape)
                         
-                        print("self gradient: after")
-                        print(self.gradient_weights[k, c])
+                        self.gradient_weights[k,c] += scipy.signal.correlate(padded_input, error_tensor[b, k], mode='valid')
+                        
+                        # print("self gradient: after")
+                        # print(self.gradient_weights[k, c])
                         gradient_input[b, c] += scipy.signal.convolve(error_tensor[b, k], self.weights[k, c], mode='full')[pad[0]:-pad[1] or None]
                     self.gradient_bias[k] += np.sum(error_tensor[b, k])
 
@@ -154,49 +175,3 @@ class Conv:
 
 
 
-
-
-
-
-# def backward(self, error_tensor):
-#     batch_size, channels, *spatial_dims = self.input_tensor.shape
-#     conv_dims = self.convolution_shape[1:]  # Get spatial dimensions of the convolution filter
-
-#     self.gradient_weights = np.zeros_like(self.weights)
-#     self.gradient_bias = np.zeros_like(self.bias)
-#     gradient_input = np.zeros_like(self.input_tensor)
-
-#     if len(conv_dims) == 1:  # 1D convolution
-#         pad = (conv_dims[0] // 2, conv_dims[0] // 2) if conv_dims[0] % 2 == 1 else (conv_dims[0] // 2, conv_dims[0] // 2 - 1)
-
-#         for b in range(batch_size):
-#             for k in range(self.num_kernels):
-#                 for c in range(channels):
-#                     padded_input = np.pad(self.input_tensor[b, c], pad, mode='constant')
-#                     self.gradient_weights[k, c] += scipy.signal.correlate(padded_input, error_tensor[b, k], mode='valid')
-#                     gradient_input[b, c] += scipy.signal.convolve(error_tensor[b, k], self.weights[k, c], mode='full')[pad[0]:-pad[1] or None]
-#                 self.gradient_bias[k] += np.sum(error_tensor[b, k])
-
-#         if self._optimizer_weights:
-#             self.weights = self._optimizer_weights.update(self.weights, self.gradient_weights)
-#         if self._optimizer_bias:
-#             self.bias = self._optimizer_bias.update(self.bias, self.gradient_bias)
-
-#     elif len(conv_dims) == 2:  # 2D convolution
-#         pad_h = (conv_dims[0] // 2, conv_dims[0] // 2) if conv_dims[0] % 2 == 1 else (conv_dims[0] // 2, conv_dims[0] // 2 - 1)
-#         pad_w = (conv_dims[1] // 2, conv_dims[1] // 2) if conv_dims[1] % 2 == 1 else (conv_dims[1] // 2, conv_dims[1] // 2 - 1)
-
-#         for b in range(batch_size):
-#             for k in range(self.num_kernels):
-#                 for c in range(channels):
-#                     padded_input = np.pad(self.input_tensor[b, c], (pad_h, pad_w), mode='constant')
-#                     self.gradient_weights[k, c] += scipy.signal.correlate2d(padded_input, error_tensor[b, k], mode='valid')
-#                     gradient_input[b, c] += scipy.signal.convolve2d(error_tensor[b, k], self.weights[k, c], mode='full')[pad_h[0]:-pad_h[1] or None, pad_w[0]:-pad_w[1] or None]
-#                 self.gradient_bias[k] += np.sum(error_tensor[b, k])
-
-#         if self._optimizer_weights:
-#             self.weights = self._optimizer_weights.update(self.weights, self.gradient_weights)
-#         if self._optimizer_bias:
-#             self.bias = self._optimizer_bias.update(self.bias, self.gradient_bias)
-
-#     return gradient_input
