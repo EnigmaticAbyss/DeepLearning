16b0a9e18cf3eedef907d829711e8a88c8899f91
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-02 21:29:09 +0200

    more rnn

--- exercise3_material/src_to_implement/Layers/RNN.py
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -1,16 +1,24 @@
 import numpy as np
 from Layers.Base import BaseLayer
+from Layers.FullyConnected import FullyConnected
 import copy
 
 class RNN(BaseLayer):
     def __init__(self, input_size, hidden_size, output_size):
+        self.FC_H = FullyConnected(hidden_size + input_size, hidden_size)
+        self.FC_Y = FullyConnected(hidden_size, output_size)
         self.trainable = True
         self.input_size = input_size
         self.hidden_size = hidden_size
         self.output_size = output_size
         self._memorize = False
-        self.weights = None
+        self.weights = self.FC_H.weights
         self.optimizer = None
+        self.weightsY = None
+        self.weightsH = None
+        self.hT = None
+        self.prevH_T = None
+        
     
     @property
     def weights(self):
@@ -34,3 +42,6 @@ class RNN(BaseLayer):
     @memorize.setter
     def memorize(self, value):
         self._memorize = value
+    def initialize(self, weights_initializer, bias_initializer):
+        self.FC_Y.initialize(copy.deepcopy(weights_initializer),copy.deepcopy( bias_initializer))
+        self.FC_H.initialize(copy.deepcopy(weights_initializer), copy.deepcopy(bias_initializer))
