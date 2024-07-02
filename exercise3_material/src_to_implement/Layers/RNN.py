54417d110cef80989960c7d4441541776e8196d6
behrooz bozorg chami <behroozbc@outlook.com>
2024-07-02 15:16:53 +0200

    start rnn

---
+++ exercise3_material/src_to_implement/Layers/RNN.py
@@ -0,0 +1,36 @@
+import numpy as np
+from Layers.Base import BaseLayer
+
+
+class RNN(BaseLayer):
+    def __init__(self, input_size, hidden_size, output_size):
+        self.trainable = True
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self._memorize = False
+        self.weights = self.FC_h.weights
+        self.optimizer = None
+    
+    @property
+    def weights(self):
+        return self._weights
+    
+    @weights.setter
+    def weights(self, weights):
+        self._weights = weights
+    @property
+    def optimizer(self):
+        return self._optimizer
+    
+    @optimizer.setter
+    def optimizer(self, optimizer):
+        self._optimizer = copy.deepcopy(optimizer)
+
+    @property
+    def memorize(self):
+        return self._memorize
+
+    @memorize.setter
+    def memorize(self, value):
+        self._memorize = value
