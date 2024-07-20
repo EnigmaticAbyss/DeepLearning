c5da045f297221339c99b481e84f59cebcb0db08
EnigmaticAbyss <arashmousavi193@gmail.com>
2024-07-20 18:51:52 +0200

    data reader

--- exercise4_material/src_to_implement/data.py
+++ exercise4_material/src_to_implement/data.py
@@ -9,7 +9,44 @@ import torchvision as tv
 train_mean = [0.59685254, 0.59685254, 0.59685254]
 train_std = [0.16043035, 0.16043035, 0.16043035]
 
-
 class ChallengeDataset(Dataset):
-    # TODO implement the Dataset class according to the description
-    pass
+    def __init__(self, data, mode: str):
+        self.data = data
+        self.mode = mode
+      
+
+        if self.mode == 'train':
+            self.transform = tv.transforms.Compose([
+                tv.transforms.ToPILImage(),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(mean=train_mean, std=train_std),
+            ])
+        else:  # mode == 'val'
+            self.transform = tv.transforms.Compose([
+                tv.transforms.ToPILImage(),
+                tv.transforms.ToTensor(),
+                tv.transforms.Normalize(mean=train_mean, std=train_std),
+            ])
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        # Get image path and label from the dataframe
+        img_path = self.data.iloc[index, 0]
+        label = self.data.iloc[index, 1]
+        
+        # Read image and convert to RGB
+        image = imread(img_path)
+        if image.ndim == 2:  # if grayscale
+            image = gray2rgb(image)
+        image = np.array(image, dtype=np.uint8)
+        
+        # Apply transformations
+        if self.transform:
+            image = self.transform(image)
+        
+        # Convert label to tensor
+        label = torch.tensor(label, dtype=torch.long)
+        
+        return image, label
